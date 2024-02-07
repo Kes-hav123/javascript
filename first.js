48d730c2f0a5053197cf17d3d8b0162d3a16131f
Kes-hav123 <157841026+Kes-hav123@users.noreply.github.com>
2024-02-07 22:11:41 +0530

    Add files via upload

---
+++ first.js
@@ -0,0 +1,30 @@
+console.log("Hello java");
+// console.log(2+3)
+// price=99.9
+// console.log(price)
+
+// const profile={
+//     userName: "keshav_sharrmaa",
+//     isFollow: false,
+//     follower: 487,
+//     folowing: 430
+// };
+// console.log(profile);
+
+// let a=10;
+// let b='10';
+// console.log(a===b);
+
+// alert("hello");
+// let name=prompt("hey what your name!");// temp way to take input from user.
+// console.log("hello " +name);
+
+// array******* fn/concat(),push(),pop(),shit(),unshift(),slice(),splice()
+
+//****************function***********
+
+function printDetail(){
+    console.log("name= Keshav Sharma");
+    console.log("age=20");
+}
+printDetail(); 
